088647d85484df01765ce32464570093b882b3ca
anixel <45008199+andifg@users.noreply.github.com>
2024-11-29 08:17:45 +0100

    inital commit

---
+++ artemis_frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.scss'
+
+import { Button } from './components/ui/button'
+
+
+function App() {
+
+  return (
+    <>
+      <div className="app">
+        <Button>Hello world</Button>
+      </div>
+    </>
+  )
+}
+
+export default App
