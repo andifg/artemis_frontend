29245b883a78b69e8678cfa848d7b32a4b8102fc
anixel <45008199+andifg@users.noreply.github.com>
2025-01-11 23:18:06 +0100

    reuse as gitos repo

--- src/client/config.ts
+++
@@ -1,7 +0,0 @@
-export type APIConfig = {
-  baseURL: string;
-};
-
-export const API: APIConfig = {
-  baseURL: window.location.origin,
-};
--- src/components/layout/layout.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./layout.scss";
-
-function Layout({ children }: { children: React.ReactNode }) {
-  return <div className="root-layout">{children}</div>;
-}
-
-export { Layout };
